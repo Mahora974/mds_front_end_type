fd5fed90040a3fc7e5f109bd87d3235ce5240ae6
Mahora Grolleau <hiphopfraise@gmail.com>
2025-10-03 16:45:07 +0200

    Feat : save profile in local storage

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -5,6 +5,6 @@
     <p>Ce projet de PWA consiste à  dévélopper une application de messagerie instantané en TypeScript obligatoirement.
       Elle doit fonctionner en PWA, et en mode offline pour la recherche de conversation ou de pièces jointes.</p>
     
-    <p>Elle est développée en Nuxt, et  utilise certbot pour le certificat ssh</p>
+    <p>Elle est développée en Nuxt, et  utilise certbot pour le certificat ssl</p>
   </div>
 </template>

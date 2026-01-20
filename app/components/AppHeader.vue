c85678f661a6649215bce443e139f58f4e852025
Mahora Grolleau <hiphopfraise@gmail.com>
2026-01-20 09:57:44 +0100

    Feat (Header): add geolocalisation

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
   import { useBattery, useOnline } from '@vueuse/core'
+  import { useGeolocation } from '@vueuse/core'
 
   const online = useOnline()
   const room = ref(import.meta.client?localStorage.getItem('room'):null)
   const { charging, chargingTime, dischargingTime, level } = useBattery()
   const batteryIcon = ref('');
+  const { coords, locatedAt, error, resume, pause } = useGeolocation()
+  const address = ref('')
 
   function determineBatteryIcon(level:number, charging:boolean){
     let battery = level*100;
@@ -17,9 +20,25 @@
       batteryIcon.value = `ic:twotone-battery${charge}-${Math.floor(battery/10)}0`
     }
   }
+
+  async function determineCity(coords: {readonly accuracy: number; readonly altitude: number | null; readonly altitudeAccuracy: number | null;
+    readonly heading: number | null;
+    readonly latitude: number;
+    readonly longitude: number;
+    readonly speed: number | null;
+}){
+    const {longitude, latitude} = coords
+    const res = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=AIzaSyB28h3dTAeiwgMFAOFQoexmB9obhpmeBoE`);
+    const response = await res.json();
+    return `${response.results[0].address_components[2].long_name}, ${response.results[0].address_components[5].long_name}`
+  }
   
   watch(level, async (newLevel) => {
     determineBatteryIcon(newLevel, charging.value)
+  })
+
+  watch(coords, async (newCoords) => {
+    address.value = await determineCity(coords.value)
   })
   
   watch(charging, async (newCharging) => {
@@ -27,11 +46,14 @@
   })
 </script>
 <template>
-  <div class="grid grid-cols-[10fr_1fr]">
-    <h1 class="text-center text-lg font-bold">Titre</h1>
+  <div class="grid grid-cols-[1fr_10fr_1fr]">
     <div>
+      <div>{{ address }}</div>
       <div class="flex" v-if="online"> <Icon name="ic:baseline-check-circle" class="text-green-900" /> En Ligne </div>
       <div class="flex" v-else><Icon name="ic:outline-cancel" class="text-grey-50" />Hors Ligne</div>
+    </div>
+    <h1 class="text-center text-lg font-bold">Titre</h1>
+    <div>
       <div><Icon :name="batteryIcon" class="px-4" />{{level * 100}} %</div>
     </div>
   </div>
